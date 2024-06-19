4a1d2c4aeb803238d56c179deb858c3cd8df7dad
Merle Makoben <merle.makoben@neuefische.de>
2024-06-19 10:55:14 +0200

    add student data for slides

--- src/ions/data/students.js
+++ src/ions/data/students.js
@@ -2,56 +2,60 @@ const students = [
 	{
 		// The ID is used to find images and videos
 		// ❗️
-		id: "diy",
+		id: "schlaufuchs",
 		// Name displayed on slide
-		app: "DIY.",
-		name: ["Charline, ", "Jana, ", "Oliver"],
+		app: "SchlauFuchs",
+		name: ["Marie, ", "Nicole, ", "Vanessa"],
 		// Optional, if set the Domain will be shown on the slide
 		// domain: "www.we-and.me",
 		// Will be printed as written here
 		techStack: [
-			"react",
-			"next.js",
+			"React",
+			"Next.js",
 			"Styled Components",
-			"Fuse.js",
-			"Cloudinary",
+			"NextAuth.js",
+			"MongoDB",
+			"Mongoose",
+			"SWR",
+			"Jest",
+			"React Testing Library",
 			"React-Toastify",
 		],
 		// Optional
 		ci: {
-			themeColor: "#f2f2f2",
+			themeColor: "#FFFFFF",
 			colors: {
-				primary: "#f2f2f2",
-				secondary: "#282828",
-				foo: "#ddf22d",
-				baz: "#FB4A5A",
+				primary: "#DF8735",
+				secondary: "#e9e9e9",
+				tertary: "#959995",
 			},
 		},
-		commits: 21,
-		pullRequests: 4,
+		commits: 609,
+		pullRequests: 83,
 		// Currently, only mobile is supported
 		device: "mobile",
 	},
 	{
 		// The ID is used to find images and videos
-		id: "stranger_plants",
+		id: "plotdata",
 		// Name displayed on slide
-		app: "Stranger Plants",
-		name: ["Fabian, ", "Henrik, ", "Ricardo"],
+		app: "Plotdata",
+		name: ["Catherine, ", "Maria, ", "Yavor"],
 		// Optional, if set the Domain will be shown on the slide
 		//domain: "www.we-and.me",
 		// Will be printed as written here
 		techStack: [
-			"react",
-			"next.js",
+			"React",
+			"Next.js",
 			"Styled Components",
-			"useSWR",
-			"Node.js",
-			"MongoDB Atlas",
-			"mongoose",
-			"Cloudinary",
 			"NextAuth.js",
-			"React-Toastify",
+			"MongoDB",
+			"Mongoose",
+			"SWR",
+			"Jest",
+			"Plotly.js",
+			"React-Plotly.js",
+			"MUI",
 		],
 		// Optional
 		ci: {
@@ -60,55 +64,94 @@ const students = [
 			// Optional, used for color-swatches on the slide
 			colors: {
 				// Keys can have any name
-				primary: "#e3f5f5",
-				foo: "#ABD1C5",
-				secondary: "#A67C53",
-				bar: "#87CEEB",
-				// baz: "#0000ff",
+				primary: "#1F77B4",
+				bar: "#03BFFF",
+				secondary: "#F8F8F8",
 			},
 		},
-		commits: 21,
-		pullRequests: 4,
+		commits: 682,
+		pullRequests: 23,
 		// Currently, only mobile is supported
 		device: "mobile",
 	},
 	{
 		// The ID is used to find images and videos
-		id: "pocket-pets",
+		id: "ruh",
 		// Name displayed on slide
-		app: "Pocket Pets",
-		name: ["Krischan, ", "Markus, ", "Nina"],
+		app: "Ruh",
+		name: ["Jan-Philipp, ", "Mathis, ", "Niko, ", "Ramin, ", "Sevket"],
 		// Optional, if set the Domain will be shown on the slide
 		//domain: "www.we-and.me",
 		// Will be printed as written here
 		techStack: [
-			"react",
-			"next.js",
+			"React",
+			"Next.js",
 			"Styled Components",
-			"useSWR",
-			"Node.js",
-			"MongoDB Atlas",
-			"mongoose",
-			"zustand",
 			"NextAuth.js",
-			"kaboom.js",
+			"MongoDB",
+			"Mongoose",
+			"SWR",
+			"Three.js",
+			"React Three Fiber",
+			"Framer Motion",
+			"React H5 Audio Player",
 		],
 		// Optional
 		ci: {
 			// Optional, used to fill the gap around the notch on certain devices
-			themeColor: "#D1C0B8",
+			themeColor: "#000000",
 			// Optional, used for color-swatches on the slide
 			colors: {
 				// Keys can have any name
-				foo: "#F7F5F1",
-				primary: "#BC8B5E",
-				secondary: "#048080",
-
-				// baz: "#0000ff",
+				foo: "#CD7273",
+				bar: "#B6A660",
+				secondary: "#779962",
+				primary: "#7190D4",
+				baz: "#9265BD",
 			},
 		},
-		commits: 21,
-		pullRequests: 4,
+		commits: 559,
+		pullRequests: 97,
+		// Currently, only mobile is supported
+		device: "mobile",
+	},
+	{
+		// The ID is used to find images and videos
+		id: "pinandjoin",
+		// Name displayed on slide
+		app: "Pin&Join ",
+		name: ["Annette, ", "Bruno, ", "Lea, ", "Mareike"],
+		// Optional, if set the Domain will be shown on the slide
+		//domain: "www.we-and.me",
+		// Will be printed as written here
+		techStack: [
+			"React",
+			"Next.js",
+			"Styled Components",
+			"NextAuth.js",
+			"MongoDB",
+			"Mongoose",
+			"SWR",
+			"Cloudinary",
+			"Leaflet",
+			"React-Toastify",
+		],
+		// Optional
+		ci: {
+			// Optional, used to fill the gap around the notch on certain devices
+			themeColor: "#FE7D5C",
+			// Optional, used for color-swatches on the slide
+			colors: {
+				// Keys can have any name
+				baz: "#FF9957",
+				foo: "#F68D58",
+				bar: "#FE7D5C",
+				secondary: "#FF735D",
+				bai: "#FF6061",
+			},
+		},
+		commits: 1019,
+		pullRequests: 94,
 		// Currently, only mobile is supported
 		device: "mobile",
 	},
